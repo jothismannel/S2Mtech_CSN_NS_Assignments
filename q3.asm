5a04fda83d25e76436cdd5db7c08a1c24f111663
jothismannel <jothisreghu@gmail.com>
2013-02-07 23:02:01 +0530

    Update q3.asm

--- q3.asm
+++ q3.asm
@@ -1,32 +1,36 @@
+;Assembly program to check whether a person is eligible for voting or not
+;Written by Jothis
+
+
+
 section .data
-        age: dd 18
-        age2: dd 0
+        age: dd 18 
+        age2: dd 0 
         msg1: db "person is eligible for voting",10,0
         msg2:db "person not eligibile for voting",10,0
-  msg3:db "%d"
+  	msg3:db "%d"
 section .text
         global main
 
 main:
-        push ebp
+        push ebp;saving base pointer
         mov ebp,esp
         ;sub esp,0x10
 
-        ;mov DWORD [ebp-4],10
-        push DWORD age2
-	push msg3
-	call scanf
-;	add esp,8
-	mov eax,[age2];DWORD PTR[ebp-4]
-        cmp eax,[age]
+        push DWORD age2;pushing first argument
+	push msg3  ;pushing second argument
+	call scanf;calling scanf
+
+	mov eax,[age2] ;moving age to eax register
+        cmp eax,[age] ;comparing the ages
         jge .label
-        push msg2
+        push msg2  ;display the persin is not eligible
         call printf;
         jmp .done
 
         .label:
                 push msg1;
-                call printf;
+                call printf; display the persin is eligible
                 jmp .done
 
         .done:
