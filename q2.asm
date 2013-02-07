64434639367ba721520e14ceab788cdec03634d6
jothismannel <jothisreghu@gmail.com>
2013-02-07 22:56:54 +0530

    Update q2.asm

--- q2.asm
+++ q2.asm
@@ -1,3 +1,8 @@
+;Assembly program to display helloworld using system calls
+;Writen by Jothis
+
+
+
 section .data
 
   mesg: db "Helloworld",10,0
@@ -7,12 +12,12 @@ section .text
 	global main
 main:
 
-	mov eax,4
+	mov eax,4;stdin 
 	mov ebx,1
 
 	mov ecx,mesg
 	mov edx,strlen	
-	int 80h
+	int 80h;interrupt to display the message
 
 	mov eax,1
 	mov ebx,0
